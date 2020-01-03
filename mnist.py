110b07d586fc90d12764a1937925e0d7559515eb
wkim97 <48055164+wkim97@users.noreply.github.com>
2020-01-04 01:07:10 +0900

    step 2 - Define CNN, loss, and optimizer

--- mnist.py
+++ mnist.py
@@ -35,3 +35,25 @@ dataiter = iter(trainloader)
 images, labels = dataiter.next()
 print(' '.join('%s' % classes[labels[j]] for j in range(batch_size)))
 imshow(torchvision.utils.make_grid(images))
+
+#############################################################################
+# 2. Define CNN, loss, and optimizer
+#############################################################################
+class MNIST_net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
+        self.conv2 = nn.Conv2d(6, 32, 5, paddding=2)
+        self.fc1 = nn.Linear(32 * 7 * 7, 1024)
+        self.fc2 = nn.Linear(1024, 10)
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)))
+        x = F.max_pool2d(F.relu(self.conv2(x)))
+        x = x.view(-1, 32 * 7 * 7)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+net = MNIST_net()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
