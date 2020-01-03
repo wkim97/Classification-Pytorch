ad0c57b5771be7b8ee800718dd85ea0528a42d55
wkim97 <48055164+wkim97@users.noreply.github.com>
2020-01-04 01:00:25 +0900

    step 1 - get training and test data

--- mnist.py
+++ mnist.py
@@ -1,1 +1,37 @@
 import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+#############################################################################
+# 1. Get training and test data sets
+#############################################################################
+transform = transforms.Compose(
+    [transforms.ToTensor()])
+batch_size = 50
+trainset = torchvision.datasets.MNIST(
+    './data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=batch_size, shuffle=True)
+testset = torchvision.datasets.MNIST(
+    './data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=batch_size, shuffle=True)
+classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+#############################################################################
+# 1-1. Show randomly selected training images and labels
+#############################################################################
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+print(' '.join('%s' % classes[labels[j]] for j in range(batch_size)))
+imshow(torchvision.utils.make_grid(images))
