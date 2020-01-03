3fb98fa8508b16ba1d2806d29987d87b7d77d1bc
wkim97 <48055164+wkim97@users.noreply.github.com>
2020-01-04 03:13:43 +0900

    step 4 - run test data

--- mnist.py
+++ mnist.py
@@ -41,14 +41,14 @@ imshow(torchvision.utils.make_grid(images))
 #############################################################################
 class MNIST_net(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(MNIST_net, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
-        self.conv2 = nn.Conv2d(6, 32, 5, paddding=2)
+        self.conv2 = nn.Conv2d(6, 32, 5, padding=2)
         self.fc1 = nn.Linear(32 * 7 * 7, 1024)
         self.fc2 = nn.Linear(1024, 10)
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)))
-        x = F.max_pool2d(F.relu(self.conv2(x)))
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
         x = x.view(-1, 32 * 7 * 7)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
@@ -61,21 +61,55 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 #############################################################################
 # 3. Learn the model with training data
 #############################################################################
-for epoch in range(15):
+train_loss = []
+train_accu = []
+for epoch in range(10):
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         images, labels = data
-        outputs = net(images)
         optimizer.zero_grad()
+        outputs = net(images)
         loss = criterion(outputs, labels)
         loss.backward()
+        train_loss.append(loss.data)
         optimizer.step()
+        prediction = outputs.data.max(1)[1]
+        accuracy = prediction.eq(labels.data).sum() / batch_size * 100
+        train_accu.append(accuracy)
         running_loss += loss.item()
-        if i % 2000 == 0:
+        if i % 1000 == 999:
             print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
+                  (epoch + 1, i + 1, running_loss / 1000))
             running_loss = 0.0
 print('Finished Training')
 PATH = './cifar_net.pth'
 torch.save(net.state_dict(), PATH)
+# plt.plot(np.arange(len(train_loss)), train_loss)
+# plt.plot(np.arange(len(train_accu)), train_accu)
 
+#############################################################################
+# 4. Testing with the test data
+#############################################################################
+test_net = MNIST_net()
+test_net.load_state_dict(torch.load(PATH))
+correct = 0
+total = 0
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = test_net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        c = (predicted == labels).squeeze()
+        for i in range(batch_size):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+print('Accuracy of the network on the test images: %d %%'
+      % (100 * correct / total))
+for i in range(10):
+    print('Accuracy of %s %2d %%'
+          % (classes[i], 100 * class_correct[i] / class_total[i]))
