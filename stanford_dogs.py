8cd6f0e51243254ac9e4e0bdf1bab4528e65ab90
wkim97 <48055164+wkim97@users.noreply.github.com>
2020-01-04 19:58:59 +0900

    stanford_dogs and configs

---
+++ stanford_dogs.py
@@ -0,0 +1,119 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from stanford_dogs_data import dogs
+import configs
+
+#############################################################################
+# 1. Get training and test data sets
+#############################################################################
+transform = transforms.Compose([
+            transforms.RandomResizedCrop(224, ratio=(1, 1.3)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor()])
+batch_size = 50
+trainset = dogs(root=configs.imagesets,
+                train=True, cropped=False, transform=transform, download=True)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=batch_size, shuffle=True)
+testset = dogs(root=configs.imagesets,
+                train=False, cropped=False, transform=transform, download=True)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=batch_size, shuffle=True)
+classes = trainset.classes
+
+#############################################################################
+# 1-1. Show randomly selected training images and labels
+#############################################################################
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+print(' '.join('%s' % classes[labels[j]] for j in range(batch_size)))
+imshow(torchvision.utils.make_grid(images))
+
+#############################################################################
+# 2. Define CNN, loss, and optimizer
+#############################################################################
+class MNIST_net(nn.Module):
+    def __init__(self):
+        super(MNIST_net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
+        self.conv2 = nn.Conv2d(6, 32, 5, padding=2)
+        self.fc1 = nn.Linear(32 * 7 * 7, 1024)
+        self.fc2 = nn.Linear(1024, 10)
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, 32 * 7 * 7)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+net = MNIST_net()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+#############################################################################
+# 3. Learn the model with training data
+#############################################################################
+train_loss = []
+train_accu = []
+for epoch in range(10):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        images, labels = data
+        optimizer.zero_grad()
+        outputs = net(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        train_loss.append(loss.data)
+        optimizer.step()
+        prediction = outputs.data.max(1)[1]
+        accuracy = prediction.eq(labels.data).sum() / batch_size * 100
+        train_accu.append(accuracy)
+        running_loss += loss.item()
+        if i % 1000 == 999:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 1000))
+            running_loss = 0.0
+print('Finished Training')
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+# plt.plot(np.arange(len(train_loss)), train_loss)
+# plt.plot(np.arange(len(train_accu)), train_accu)
+
+#############################################################################
+# 4. Testing with the test data
+#############################################################################
+test_net = MNIST_net()
+test_net.load_state_dict(torch.load(PATH))
+correct = 0
+total = 0
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = test_net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        c = (predicted == labels).squeeze()
+        for i in range(batch_size):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+print('Accuracy of the network on the test images: %d %%'
+      % (100 * correct / total))
+for i in range(10):
+    print('Accuracy of %s %2d %%'
+          % (classes[i], 100 * class_correct[i] / class_total[i]))
